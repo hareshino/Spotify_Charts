45be9c91df3a0c8558e8eeef0cbb6ddb234bec1f
hareshino <puyopuyochan6201@gmail.com>
2023-12-01 06:55:08 +0900

    firstcommit

---
+++ SpotifyCharts_Scraping.py
@@ -0,0 +1,135 @@
+﻿import os
+import time
+import glob
+from dotenv import load_dotenv
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+#一週間前の日付を取得
+from datetime import datetime, timedelta
+
+
+# .env ファイルの内容を読み込む
+load_dotenv()
+
+# カレントディレクトリの取得
+current_dir = os.getcwd()
+# 一時ダウンロードフォルダパスの設定
+tmp_download_dir = f'{current_dir}\\tmpDownloads'
+# 一時フォルダの作成
+if not os.path.exists(tmp_download_dir):
+    os.mkdir(tmp_download_dir)
+# Chromeオプション設定でダウンロード先の変更
+options = webdriver.ChromeOptions()
+prefs = {"download.default_directory": str(tmp_download_dir)}
+options.add_experimental_option("prefs", prefs)
+
+driver_path = f'{current_dir}\\chromedriver-win64\\chromedriver.exe'
+chrome_service = Service(executable_path=driver_path)
+driver = webdriver.Chrome(service=chrome_service, options=options)
+
+
+#SpotifyChartsにログインする
+def SpotifyChartsLogin():
+    url = "https://charts.spotify.com/home"
+    #サイトのリンク
+    driver.get(url=url)
+    ##################### ここからWeb要素の探索 #####################
+    # コピーしたセレクタを文字列に格納
+    selector = "#__next > div > div > main > div.Content-sc-1n5ckz4-0.jyvkLv > div > section.ChartsHomeHero__StyledSection-sc-1vcyx6n-0.bDiUTk > div > div > div.ChartsHomeHero__HeroContent-sc-1vcyx6n-3.hEhOFo > div > a"
+    element = driver.find_element(By.CSS_SELECTOR, selector)
+    #クリック
+    element.click()
+    #ログイン画面に移行、ログインする
+    time.sleep(1)
+    username_selector = "#login-username"
+    password_selector = "#login-password"
+    username_element = driver.find_element(By.CSS_SELECTOR, username_selector)
+    password_element = driver.find_element(By.CSS_SELECTOR, password_selector)
+
+    #.envからusernameとpasswordを取得
+    username=os.getenv('SP_USERNAME')
+    password=os.getenv('SP_PASSWORD')
+    username_element.send_keys(username)
+    password_element.send_keys(password)
+    time.sleep(1)
+    password_element.send_keys(Keys.RETURN)  # Enterキーを押してログイン
+
+    time.sleep(3)
+
+
+
+#SpotifyChartsからcsvをダウンロードする
+def SpotifyChartsCSV(url: str):
+    #サイトのリンク
+    time.sleep(2)
+    driver.get(url=url)
+    time.sleep(2)
+    ##################### ここからWeb要素の探索 #####################
+    # コピーしたセレクタを文字列に格納
+    selector = "#__next > div > div > main > div.Content-sc-1n5ckz4-0.jyvkLv > div:nth-child(3) > div > div > a > button"
+    element = driver.find_element(By.CSS_SELECTOR, selector)
+
+    try:
+        # セレクターを指定
+        selector = '#__next > div > div > main > div.Content-sc-1n5ckz4-0.jyvkLv > div:nth-child(3) > div > div > a > button'
+
+        # セレクターがクリック可能になるまで待機
+        element = WebDriverWait(driver, 3).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, selector))
+        )
+
+        # 要素をクリック
+        element.click()
+
+    except Exception as e:
+        print(f"エラーが発生しました: {e}")
+
+
+# SpotifyChartsCSV(url)
+SpotifyChartsLogin()
+
+def get_last_thursday():
+    # 今日の日付を取得
+    today = datetime.now()
+    # 今日が何曜日かを取得 (0: 月曜日, 1: 火曜日, ..., 6: 日曜日)
+    current_weekday = today.weekday()
+    # 今日から最も近い木曜日までの日数を計算
+    days_until_last_thursday = (current_weekday - 3) % 7
+    # 直近の木曜日の日付を計算
+    last_thursday = today - timedelta(days=days_until_last_thursday)
+    return last_thursday
+
+# 直近の木曜日を取得
+last_thursday = get_last_thursday()
+
+def weekly(num: int,current_date = get_last_thursday() - timedelta(55*7)):
+    
+    for i in range(num):
+        url = f'https://charts.spotify.com/charts/view/regional-jp-weekly/{current_date.strftime("%Y-%m-%d")}'
+        SpotifyChartsCSV(url)
+        # 前の1週間
+        current_date = current_date - timedelta(days=7)
+
+def daily(num: int,current_date = datetime.now() - timedelta(days=2+483)):
+    for i in range(num):
+        url = f'https://charts.spotify.com/charts/view/regional-jp-daily/{current_date.strftime("%Y-%m-%d")}'
+        SpotifyChartsCSV(url)
+        # 前の1週間
+        current_date = current_date - timedelta(days=1)
+
+# 現在の日付 - 1を取得
+# current_date = datetime.now()
+# 1週間に合わせる
+# current_date = current_date - timedelta(days=8)
+weekly(48)
+
+#閉じる
+time.sleep(5)
+print("ダウンロード完了")
+driver.quit()
---
+++ test.py
@@ -0,0 +1,145 @@
+﻿import pandas as pd
+import requests
+import time
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+# .envファイルの内容を読み込見込む
+load_dotenv()
+
+# Spotify Developer Dashboardで取得したクライアントIDとクライアントシークレット
+client_id = os.environ['CLIENT_ID']
+client_secret = os.environ['CLIENT_SECRET']
+
+# 認証用のエンドポイント
+auth_url = 'https://accounts.spotify.com/api/token'
+
+# 認証用の情報を作成
+auth_data = {
+    'grant_type': 'client_credentials',
+    'client_id': client_id,
+    'client_secret': client_secret,
+}
+
+# アクセストークンを取得
+auth_response = requests.post(auth_url, data=auth_data)
+auth_response_data = auth_response.json()
+access_token = auth_response_data['access_token']
+
+def get_audio_features(track_id, access_token):
+    endpoint = f'https://api.spotify.com/v1/audio-features/{track_id}'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    retries=10
+    sleep_time=1
+
+    for _ in range(retries):
+        try:
+            response = requests.get(endpoint, headers=headers)
+            if response.status_code == 200:
+                audio_features = response.json()
+                return audio_features
+            elif response.status_code == 429:
+                print("API rate limit exceeded. Retrying after sleeping.(audio-features)")
+                time.sleep(sleep_time)
+            else:
+                print(f"エラー: {response.status_code}, {response.text}")
+                return None
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed: {e}")
+            return None
+
+import requests
+
+def get_track_genres(track_id, access_token):
+    endpoint = f'https://api.spotify.com/v1/tracks/{track_id}'
+    headers = {'Authorization': f'Bearer {access_token}'}
+    retries=10
+    sleep_time=1
+
+    for _ in range(retries):
+        try:
+            response = requests.get(endpoint, headers=headers)
+            if response.status_code == 200:
+                track_info = response.json()
+                artist_id = track_info['artists'][0]['id']
+            elif response.status_code == 429:
+                print("API rate limit exceeded. Retrying after sleeping.(track-id)")
+                time.sleep(sleep_time)
+            else:
+                print(f"エラー: {response.status_code}, {response.text}")
+                return None
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed: {e}")
+            return None
+    
+    # アーティストの詳細情報を取得
+    endpoint = f'https://api.spotify.com/v1/artists/{artist_id}'
+    response = requests.get(endpoint, headers=headers)
+    for _ in range(retries):
+        try:
+            if response.status_code == 200:
+                artist_data = response.json()
+                genres = artist_data['genres']
+                return genres
+            elif response.status_code == 429:
+                print("API rate limit exceeded. Retrying after sleeping.(artist-id)")
+                time.sleep(sleep_time)
+            else:
+                print(f"Error: {response.status_code}, {response.text}")
+                return None
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed: {e}")
+            return None
+
+# トラックIDとアクセストークンを指定(要取得！)
+# access_token = ''
+
+def getInfo(current_date: datetime):
+
+    dfsongs = pd.read_csv(f'./Weekly_2023/regional-jp-weekly-{current_date.strftime("%Y-%m-%d")}.csv',encoding='utf-8-sig')
+
+    song_info = []
+
+    # 'uri'行だけを取り出す
+    uri = dfsongs.iloc[:,1]
+    remove = "spotify:track:"
+
+    i=0
+    for url in uri:
+        id = url.replace(remove,"")
+        data = get_audio_features(id, access_token)
+        genre = get_track_genres(id, access_token)
+        #genre追加
+        print(i)
+        i = i + 1
+        data['artist_genre'] = genre
+        data['date'] = current_date.strftime("%Y-%m-%d") #日付を追加 2023-11-23なら2023-11-17から2023-11-23までのデータ
+        song_info.append(data)
+    dfinfo = pd.DataFrame(song_info)
+
+    dfinfo.to_csv('apiData.csv',index=True, encoding='utf_8-sig')
+
+    # 'ID'列をキーにして結合
+    merged_df = pd.concat([dfsongs,dfinfo],axis=1)
+
+    # 分析に不要な列を削除
+    merged_df = merged_df.drop(columns=['uri'])
+    df_analysis = merged_df.drop(columns=['id','analysis_url','track_href'])
+
+    #保存
+    csv_file_path = f'./data/weeks_{date.strftime("%Y-%m-%d")}.csv'
+    merged_df.to_csv(csv_file_path, index=True, encoding='utf_8-sig')
+
+    csv_file_path = f'./data_analysis/spotify_top200_jp_weeks_{date.strftime("%Y-%m-%d")}.csv'
+    df_analysis.to_csv(csv_file_path, index=True, encoding='utf_8-sig')
+    
+date = datetime.now() - timedelta(22)
+for i in range(102):
+    print(f'取得開始:{date.strftime("%Y-%m-%d")}')
+    getInfo(date)
+    date = date - timedelta(7)
+    print(f'取得完了:{date.strftime("%Y-%m-%d")}')
+    time.sleep(1)
